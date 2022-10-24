c041ed33f0e70e1f6152ac5de705d6a50c532a26
just2102 <thesixteensaltines@gmail.com>
2022-10-25 08:20:05 +0900

    counter working as intended, reset button added.

--- script.js
+++ script.js
@@ -10,29 +10,39 @@ function getComputerChoice() {
     }
     return computerSelection;
 }
-
+//variables set to record score set outside of the playGame function
+let scoreYou    =   0;
+let scoreComputer   =   0;
 
 function playGame(playerSelection, computerSelection) {
     computerSelection   =   getComputerChoice();
     let result=''
     if (computerSelection ==  'rock' && playerSelection == 'paper') {
         result+= 'You threw ' + playerSelection + '. computer threw ' + computerSelection + '. you won';
+        scoreYou++
     } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
         result+= 'You threw ' + playerSelection + '. computer threw ' + computerSelection + '. you lost';
+        scoreComputer++
 
     } else if (computerSelection == 'paper' && playerSelection == 'rock') {
         result+= 'You threw ' + playerSelection + '. computer threw '+computerSelection + '. you lost';
+        scoreComputer++
     } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
         result+= 'You threw ' + playerSelection + '. computer threw '+computerSelection + '. you won';
-    
+        scoreYou++
+
     } else if (computerSelection    ==  'scissors' && playerSelection   ==  'rock') {
         result+= 'You threw ' + playerSelection + '. computer threw '+computerSelection + '. you won';
+        scoreYou++
     } else if (computerSelection    ==  'scissors' && playerSelection   ==  'paper') {
         result+= 'You threw ' + playerSelection + '. computer threw '+computerSelection + '. you lost';
+        scoreComputer++
 
     }else if (computerSelection   ==  playerSelection) {
         result+= 'A draw!';
     }
+document.getElementById('you_number').innerText =   0+  scoreYou
+document.getElementById('computer_number').innerText    =   0+scoreComputer;
 // outputs computer choice
     let compField   =   document.getElementById('computer_choice');
     if (computerSelection   ==  'rock') {
@@ -48,7 +58,7 @@ function playGame(playerSelection, computerSelection) {
     if (result== 'You threw ' + playerSelection + '. computer threw ' + computerSelection + '. you won') {
         resultField.classList.remove('draw');
         resultField.classList.remove('loss');
-        resultField.classList.add('win')
+        resultField.classList.add('win');
     } else if (result== 'You threw ' + playerSelection + '. computer threw ' + computerSelection + '. you lost') {
         resultField.classList.remove('win');
         resultField.classList.remove('draw');
@@ -59,7 +69,14 @@ function playGame(playerSelection, computerSelection) {
         resultField.classList.add('draw')
     }
     resultField.innerText   =   result;    
-
+    console.log(scoreYou);
 }
 
-
+// reset button
+function reset() {
+    scoreYou    =   0;
+    scoreComputer = 0;
+    document.getElementById('you_number').innerText =   0;
+    document.getElementById('computer_number').innerText    =   0;
+    document.getElementById('result_field').innerText   =   ''
+}
